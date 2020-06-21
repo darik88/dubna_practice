78466fe346dc077965bd043fb31e1752346cbd3b
darik88 <mr.darik88@gmail.com>
2020-06-21 20:11:18 +0300

    attempt to run test in parallel

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,14 @@ from pages.loginPage import LoginPage
 
 CONFIG_PATH = './configs/config.json'
 
+
+@pytest.fixture
+def config():
+    with open(CONFIG_PATH) as config_file:
+        data = json.load(config_file)
+        return data
+
+
 capabilities = {
     "browserName": "firefox",
     "version": "76.0",
@@ -12,29 +20,32 @@ capabilities = {
     "enableVideo": False
 }
 
+capabilities2 = {
+    "browserName": "chrome",
+    "version": "83.0",
+    "enableVNC": True,
+    "enableVideo": False
+}
+
+browsers = [capabilities, capabilities2]
+
 
 @pytest.fixture
 def browser(config_base_url):
-    browser = webdriver.Remote(
-        command_executor="http://192.168.1.100:4444/wd/hub",
-        desired_capabilities=capabilities)
-    browser.maximize_window()
-    browser.get(config_base_url)
-    yield browser
-    browser.quit()
+    for elem in browsers:
+        browser = webdriver.Remote(
+            command_executor="http://192.168.1.100:4444/wd/hub",
+            desired_capabilities=elem)
+        browser.maximize_window()
+        browser.get(config_base_url)
+        yield browser
+        browser.quit()
 
 
 @pytest.fixture
 def correct_login(browser, config_user_name, config_user_password):
     login_page = LoginPage(browser)
     login_page.login(config_user_name, config_user_password)
-
-
-@pytest.fixture
-def config():
-    with open(CONFIG_PATH) as config_file:
-        data = json.load(config_file)
-        return data
 
 
 @pytest.fixture
